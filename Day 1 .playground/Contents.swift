54baf5c0b69c9a46ad05c017284da7e69d45a46f
tammy0508 <tamara.dandreamatteo@gmail.com>
2019-07-08 14:52:55 -0700

    Initial commit

---
+++ Day 1 .playground/Contents.swift
@@ -0,0 +1,85 @@
+import UIKit
+
+var str = "Hello, playground"
+var age = 16
+"I am \(age) years old"
+let string1 = "hello"
+let string2 = "there"
+var welcome = "\(string1) \(string2)"
+var anotherWelcome = string1 + " " + string2
+print ("Hello, World!")
+var first = "Karlie"
+var last = "Kloss"
+print("\(first)\(last)")
+print("\(last)\(first)")
+print("\(first) \(last)")
+print("\(last) \(first) \(last) \(first)")
+print("I love \(first)")
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+print((a + b + c + d) / 4)
+let name = "Amy"
+var address = "49 Los Felis Dr"
+let birthday = "5/12/2003"
+var gradeLevel = "11"
+let eyeColor = "brown"
+var favoriteColor = "blue"
+var e = 10.25
+var f = 20
+print(Double(f) * e)
+print("Happy Birthday \(first) \(last)")
+var StringtoCompress = "KarlieKloss"
+print("\(StringtoCompress.first!)\(StringtoCompress.count-2)\(StringtoCompress.last!)")
+5 < 3
+12 > 7
+6 != 8
+7 == 7
+"karlie" == "karlie"
+"karlie" == "karlie"
+"Karlie" == "karlie"
+var luckyNum = 7
+luckyNum < 0
+luckyNum == 7
+var dogAge = 1.2
+if dogAge < 2 {
+    print("You are a puppy :)")
+}
+else if dogAge > 12 {
+    print("You are elderly <3")
+}
+else {
+    print("You are also awesome...")
+}
+var favoriteFood = "Poke"
+if favoriteFood == "Chipotle" {
+    print("You're wrong >:(")
+}
+else if favoriteFood == "Starbucks" {
+    print("And that's the tea...")
+}
+else if favoriteFood == "Poke" {
+    print("That's right :)")
+}
+else {
+    print("Go get some poke !!!")
+}
+var hasAccount = false
+if hasAccount == false {
+    print("Let's get some information to create an account for you.")
+}
+else {
+    print("Please log in")
+}
+var grade = 11
+if grade < 6 {
+    print("babies")
+}
+if grade > 8 {
+    print("4 years of anxiety")
+}
+else {
+    print("cringe")
+}
+
